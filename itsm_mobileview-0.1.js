88907439c0b77bb1f5d242ee1b233574de6cd72b
syvo <sylvain.yvon@gmail.com>
2012-11-04 13:19:16 +0100

    added the Connect service; known issue with the way the variables are set from the namespaced functions ... working on it

--- itsm_mobileview-0.1.js
+++ itsm_mobileview-0.1.js
@@ -1,5 +1,5 @@
 /*
-*	Copyright (C) 2013 Sylvain YVON
+*	Copyright (C) 2013
 *	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 *	The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -8,6 +8,15 @@
 	name: 'ITSM_MOBILEVIEW',
 	context: '/arsys',
 	callService: function(credentials,service,successFn,errorFn)
+	/* callService : calls a service from the MOB:UserCentral AR System web service [RefreshCounters|Connect]
+			credentials : array
+				"username" : Remedy login name
+				"password" : AR System password, as it would be typed on MT login page (either for external authentication or not)
+				"server" : the server name as appearing in MT configuration page
+			service : service name
+			successFn : handler for the "success" ajax event
+			errorFn : handler for the "error" ajax event
+	*/
 	{
 		console.log(ITSM_MOBILEVIEW.name + ' [INFO] callService - service ' + service);
 		var wsURL = ITSM_MOBILEVIEW.context + '/services/ARService?server=' + credentials.server + '&webService=MOB:UserCentral';
@@ -21,7 +30,7 @@
 						   </soapenv:Header>\
 						   <soapenv:Body>\
 							  <urn:' + service + '>\
-								 <urn:z1D_serviceName>' + service + '</urn:Qualification>\
+								 <urn:z1D_serviceName>' + service + '</urn:z1D_serviceName>\
 							  </urn:' + service + '>\
 						   </soapenv:Body>\
 						</soapenv:Envelope>';
@@ -39,10 +48,12 @@
 			contentType: "text/xml; charset=\"utf-8\"",				
 			success: function(xml){
 				console.log(ITSM_MOBILEVIEW.name + ' [INFO]   ...success');
-				successFn(xml);},
+				successFn(xml);
+				return true;},
 			error: function(error){
 				console.log(ITSM_MOBILEVIEW.name + ' [ERROR]   ...error!');
-				errorFn(error);}
+				errorFn(error);
+				return false;}
 		});
 		return false;
 	},
@@ -96,14 +107,33 @@
 		$(xml).find("ns0\\:getListValues").each(function()
 		{
 			userinfo = {
-				fullname: $(this).find("ns0\\:Full_Name").text(),
-				license_type: $(this).find("ns0\\:License_Type").text(),
-				email: $(this).find("ns0\\:Email_Address").text(),
-				groupids: $(this).find("ns0\\:Group_List").text()
+				fullname: $(this).find("ns0\\:fullName").text(),
+				license_type: $(this).find("ns0\\:licenseType").text(),
+				email: $(this).find("ns0\\:emailAddress").text(),
+				groups: $(this).find("ns0\\:groupList").text()
 			};
 			console.log(ITSM_MOBILEVIEW.name + ' [INFO]   found user ' + userinfo.fullname);
 		});
 		return userinfo;
+	},
+	extractCounters: function(xml) {
+		var counters;
+		console.log(ITSM_MOBILEVIEW.name + ' [INFO] extractCounters');
+		$(xml).find("ns0\\:getListValues").each(function()
+		{
+			counters = {
+				countIncidents: $(this).find("ns0\\:countIncidents").text(),
+				countChanges: $(this).find("ns0\\:countChanges").text(),
+				countTasks: $(this).find("ns0\\:countTasks").text(),
+				countSignatures: $(this).find("ns0\\:countSignatures").text()
+			};
+			console.log(ITSM_MOBILEVIEW.name + ' [INFO]   found counters ' 
+				+ counters.countIncidents + "/" 
+				+ counters.countChanges + "/" 
+				+ counters.countTasks + "/" 
+				+ counters.countSignatures);
+		});
+		return counters;
 	}
 }
 console.log(ITSM_MOBILEVIEW.name + ' [INFO] namespace loaded');
